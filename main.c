77a6c808199629d02f3fe8932e440485f0e0f3e6
LuckyDonpad <superlalko31@gmail.com>
2022-02-04 22:26:18 +0300

    переписать на вайлах

--- main.c
+++ main.c
@@ -1,6 +1,133 @@
 #include <stdio.h>
 
+// ввод в масив array с клавиатуры size элементов
+void inputArray(int *array, size_t size) {
+    for (int pos = 0; pos < size; ++pos)
+        scanf("%d", &array[pos]);
+}
+
+// вывод size элементов масива array;
+void outputArray(int *array, size_t size) {
+    for (int pos = 0; pos < size; ++pos)
+        printf("%d ", array[pos]);
+}
+
+// ввод входных данных для задачи и ее номер
+// task - номер задачи
+// arrayA - массив A
+// arrayB - массив B
+// sizeA - количество элемнтов массива arrayA
+// sizeB - количество элемнтов массива arrayB
+void setTaskCondition(size_t *task, int *arrayA, int *arrayB, size_t *sizeA, size_t *sizeB) {
+    puts("Input task number");
+    scanf("%lld", task);
+
+    puts("Input number of elements for arrayA < 100");
+    scanf("%lld", sizeA);
+    puts("Input arrayA");
+    inputArray(arrayA, *sizeA);
+
+    puts("Input number of elements for arrayB < 100");
+    scanf("%lld", sizeB);
+    puts("Input arrayB");
+    inputArray(arrayB, *sizeB);
+}
+
+//Дано:
+//
+//А – массив натуральных чисел, в котором нет одинаковых элементов;
+//
+//В – массив натуральных чисел, в котором нет одинаковых элементов.
+//
+//Получить массив С, содержащий все элементы массивов А и В без повторений.
+
+void task1(int const *arrayA, const int *arrayB, int *arrayC, const size_t *sizeA, const size_t *sizeB, size_t *sizeC) {
+    for (int i = 0; i < *sizeA; ++i) {
+        int isUnique = 1;
+        for (int j = 0; j < *sizeC; ++j) {
+            if (arrayA[i] == arrayC[j])
+                isUnique = 0;
+        }
+        if (isUnique) {
+            arrayC[*sizeC] = arrayA[i];
+            *sizeC += 1;
+        }
+    }
+
+    for (int i = 0; i < *sizeB; ++i) {
+        int isUnique = 1;
+        for (int j = 0; j < *sizeC; ++j) {
+            if (arrayB[i] == arrayC[j])
+                isUnique = 0;
+        }
+        if (isUnique) {
+            arrayC[*sizeC] = arrayB[i];
+            *sizeC += 1;
+        }
+    }
+}
+
+//Дано:
+//
+//А – массив натуральных чисел, в котором нет одинаковых элементов;
+//
+//В – массив натуральных чисел, в котором нет одинаковых элементов.
+//
+//Получить массив С, содержащий все такие элементы, которые есть и в массиве А и в массиве В.
+void task2(int const *arrayA, const int *arrayB, int *arrayC, const size_t *sizeA, const size_t *sizeB, size_t *sizeC) {
+    for (int i = 0; i < *sizeA; ++i) {
+        int isUnique = 1;
+        for (int j = 0; j < *sizeB; ++j) {
+            if (arrayA[i] == arrayB[j])
+                isUnique = 0;
+        }
+        if (!isUnique) {
+            int isSubUnique = 1;
+            for (int k = 0; k < *sizeC; ++k) {
+                if(arrayA[i] == arrayC[k])
+                    is
+            }
+            arrayC[*sizeC] = arrayA[i];
+            *sizeC += 1;
+        }
+    }
+}
+
+
+
+void doTask(const size_t task, int *arrayA, int *arrayB, int *arrayC, size_t *sizeA, size_t *sizeB, size_t *sizeC) {
+    switch (task) {
+        case 1:
+            task1(arrayA, arrayB, arrayC, sizeA, sizeB, sizeC);
+            break;
+        case 2:
+            task2(arrayA, arrayB, arrayC, sizeA, sizeB, sizeC);
+            break;
+        case 3:
+            puts("task 3");
+            break;
+        case 4:
+            puts("task 4");
+            break;
+        default:
+            break;
+    }
+}
+
+
 int main() {
-    printf("Hello, World!\n");
+    size_t capacity = 100;
+    size_t sizeA, sizeB, task;
+    int arrayA[capacity];
+    int arrayB[capacity];
+    int arrayC[capacity * 2];
+    size_t sizeC = 0;
+
+    setTaskCondition(&task, arrayA, arrayB, &sizeA, &sizeB);
+
+    doTask(task, arrayA, arrayB, arrayC, &sizeA, &sizeB, &sizeC);
+
+    outputArray(arrayC, sizeC);
+
     return 0;
 }
