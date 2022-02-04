d5e1c5bb74dad978a05c2b896b4710415d0e5815
LuckyDonpad <superlalko31@gmail.com>
2022-02-04 20:31:30 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/dm_0_0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dm_0_0.dir/main.c.obj"
+  "dm_0_0.exe"
+  "dm_0_0.exe.manifest"
+  "dm_0_0.pdb"
+  "libdm_0_0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dm_0_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
